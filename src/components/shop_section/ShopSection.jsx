824f750e881252179a4c7b88fb4970fc9c9aa7dc
20120578 <pqthai2502@gmail.com>
2022-12-21 22:22:22 +0700

    update Cart Page handler + api

--- src/components/shop_section/ShopSection.jsx
+++ src/components/shop_section/ShopSection.jsx
@@ -1,16 +1,34 @@
 import "./shop_section.css"
 import ShopTag from "../shop_tag/ShopTag"
 import CartItem from "../cart_item/CartItem"
+import { useState, useEffect } from "react"
 
-export default function ShopSection() {
+export default function ShopSection({ shop_data, set_total_price }) {
+    const [seller_tag, set_seller_tag] = useState({})
+    const [books, set_books] = useState([])
+
+    useEffect(() => {
+        const seller = {
+            avatar_url: shop_data?.seller_avt,
+            name: shop_data?.seller_name
+        }
+        set_seller_tag(seller)
+        set_books(shop_data?.books)
+    }, [])
+    
+    const check_handle = (e) => {
+        set_total_price(shop_data?.total_price)
+    }
+
     return (
         <div className="shop-section">
             <div className="shop-section__selector">
-                <input type="radio" name="shop-selector" className="shop-section__selector-checkbox" />
-                <ShopTag/>
+                <input type="radio" name="shop-selector" className="shop-section__selector-checkbox" onChange={check_handle}/>
+                <ShopTag user={seller_tag}/>
             </div>
-            <CartItem/>
-            <CartItem/>
+            {
+                books.map(book => <CartItem book_data={book} key={book.book_id}/>)
+            } 
         </div>
     )
 }
