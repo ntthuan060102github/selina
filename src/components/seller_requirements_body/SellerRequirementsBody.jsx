aa948a7e55424528e69d5342c6ebeac42ab35e50
Bitz0225 <pqthai2502@gmail.com>
2022-12-24 15:48:47 +0700

    Add loading effect on Admin Page

--- src/components/seller_requirements_body/SellerRequirementsBody.jsx
+++ src/components/seller_requirements_body/SellerRequirementsBody.jsx
@@ -6,12 +6,15 @@ import { useEffect, useState } from "react"
 import { Pagination } from "@mui/material"
 import HorizontalBookCard from "../horizontal_book_card/HorizontalBookCard"
 import "./seller_requirements_body.css"
+import CircularProgress from '@mui/material/CircularProgress'
+import ContentPasteOffIcon from '@mui/icons-material/ContentPasteOff'
 
 export default function SellerRequirementsBody({set_has_token}) {
     const navigate = useNavigate()
     const [books, set_books] = useState([])
     const [curr_page, set_curr_page] = useState(1)
     const [num_pages, set_num_pages] = useState(1)
+    const [loading, set_loading] = useState(true)
 
     const click_pagination_bar = function(e, page) {
         set_curr_page(page)
@@ -37,24 +40,43 @@ export default function SellerRequirementsBody({set_has_token}) {
             const books_data = response.data.data.books
             set_books(books_data)
             set_num_pages(response.data.data.pages)
+            set_loading(false)
         }
         get_data()
     }, [curr_page])
 
     return (
         <div className="seller-requirements-body">
+            <div className="seller-requirements-body__loading">
+                {loading ? <CircularProgress/> : <></>}
+            </div>
             <div className="seller-requirements-body__books">
                 {
                     books.map((book, idx) => <HorizontalBookCard key={idx} set_has_token={set_has_token} book_data={book}/>)
                 }
             </div>
-            <div className="seller-requirements-body__pagination">
-                <Pagination 
-                    count={num_pages} 
-                    variant="outlined"
-                    onChange={click_pagination_bar}
-                />
-            </div>
+            {
+                books.length
+                ? <div className="seller-requirements-body__pagination">
+                    <Pagination 
+                        count={num_pages} 
+                        variant="outlined"
+                        onChange={click_pagination_bar}
+                    />
+                </div>
+                : (
+                    !loading
+                    ? <div className="seller-requirements-body__empty">
+                        <div className="seller-requirements-body__empty-message">
+                            Không có sản phẩm nào cần kiểm duyệt!
+                        </div>
+                        <div className="seller-requirements-body__empty-icon">
+                            <ContentPasteOffIcon fontSize="large"/>
+                        </div>
+                    </div>
+                    :<></>
+                )
+            }
         </div>
     )
 }
--- src/components/user_card/UserCard.jsx
+++ src/components/user_card/UserCard.jsx
@@ -36,7 +36,33 @@ export default function UserCard({user_data}) {
                         </div>
                     </div>
                     <div className="user-card__sub-area user-card__sub-area-bot">
-
+                        <div className="user-card__buttons">
+                            {
+                                user_data?.user_data !== "banned"
+                                ? <div className="user-card__button user-card__ban-button">
+                                <div className="user-card__button-icon">
+                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M15.5 0.5L0.5 15.5" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+                                        <path d="M0.5 0.5L15.5 15.5" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+                                    </svg>
+                                </div>
+                                <div className="user-card__button-label">
+                                    Khóa tài khoản
+                                </div>
+                                </div>
+                                : <div className="user-card__button user-card__unlock-button">
+                                    <div className="user-card__button-icon">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="12" cy="12" r="11.25" stroke="white"/>
+                                            <path d="M7 12L10.75 15.75L17 8.25" stroke="white"/>
+                                        </svg>
+                                    </div>
+                                    <div className="user-card__button-label">
+                                        Mở khóa tài khoản
+                                    </div>
+                                </div>
+                            }
+                        </div>
                     </div>
                 </div>
                 : <></>
